18609b7f1d6f4571301ba0d035fdcde084c4121b
yyf <gt_yyf@126.com>
2017-12-19 00:15:11 +0800

    提交代码

---
+++ compose.js
@@ -0,0 +1,83 @@
+function a(next){
+	console.log(1);
+	next();
+}
+
+function b(next){
+	next();
+	console.log(2);
+}
+
+function c(next){
+	console.log(3);
+	next();
+}
+
+//通过promise实现
+function compose(...args){
+	let i = -1;
+	
+	var dispatch = function(i){
+		if(args[i] && typeof args[i] == 'function'){
+			//必须是函数
+			const fn = args[i];
+			if(!fn){
+				//下面没有函数了
+				return Promise.reject();
+			}
+			return Promise.resolve(fn(function(){ //生成一个proimise对象
+				return dispatch(i+1);
+			}));
+			
+			// return fn(function(){
+			// 	return dispatch(i+1);
+			// });
+		}
+	}
+	return dispatch(0);
+}
+
+var d1 = [a,b,c];
+var e1 = compose(...d1);
+
+
+/*
+	作用：
+		一个事件流
+ */
+
+function compose(args){
+	let i = -1;
+	
+	var dispatch = function(i){
+		if(args[i] && typeof args[i] == 'function'){
+			//必须是函数
+			const fn = args[i];
+			if(!fn){
+				//下面没有函数了
+				return;
+			}
+
+			//非promise版
+			return fn(function(){
+				return dispatch(i+1);
+			});
+
+			//promise版见上
+		}
+	}
+	return dispatch(0);
+}
+
+var a = function(next){
+	console.log(1);
+	next();
+}
+
+var b = function(next){
+	console.log(2);
+	next();
+}
+
+var c = [a,b];
+var d = compose(c);
--- readme.txt
+++ readme.txt
@@ -1,13 +1,22 @@
 中间件  (middlewares)  https://www.npmjs.com/package/koa-middlewares
     1、koa-router
 
-    2、生成器函数 generator【异步编程解决方案】
+    洋葱 => 
+        use,next,promise,compose
 
-    function* a() {
-        yield
-    }
+    use =>
+        compose函数式编程
 
-    1、方法的调用不执行
-    2、yield
+        思想：
+            1、将执行的顺序函数放入middleware中间件数组中
+            2、遍历middleware数组，通过Promise实现一个函数执行完后，执行middleware数组中的下一个函数。
+            3、直到没有下面没有中间件函数的时候，然后往下面继续执行
 
-    3、module
+        使用到的技术 Promise
+        使用到的思想 compose函数式编程
+
+    学习作用:
+        见compose.js
+
+
+    
